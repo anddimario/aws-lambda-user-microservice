08b2ad8c10b0d3fd13fcbc1e5d18cab54c64fb29
Andrea Di Mario <anddimario@gmail.com>
2017-12-10 15:54:07 +0100

    fix

--- services/user/editPassword.js
+++ services/user/editPassword.js
@@ -13,7 +13,7 @@ module.exports = (body, token, cb) => {
   const schema = joi.object().keys(validators);
   const validation = joi.validate(body, schema);
   if (validation.error) {
-    return cb(new Error(JSON.stringify(validation.error.details}));
+    return cb(new Error(JSON.stringify(validation.error.details)));
   }
   authorize(token, ['user'], (err, user) => {
     if (err) {
